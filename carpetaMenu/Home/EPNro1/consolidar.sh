c2c098027e8f6ae2311009a4249b6bfd1cc9e26a
ailen <apestana@fi.uba.ar>
2026-03-23 21:58:59 -0300

    Agregamos opcion en menu para borrar el Home y los procesos en segundo plano, funcionó una vez y ahora tenemos errores de   direcciones de carpetas

--- carpetaMenu/Home/EPNro1/consolidar.sh
+++ carpetaMenu/Home/EPNro1/consolidar.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-
-entrada=./entrada #Guardamos las carpetas en variables 
-salida=./salida/FILENAME.txt #Guardamos en salida el txt para luego usarlo de ubicación de destino
+entrada=./entrada
+salida=./salida/FILENAME.txt
 procesado=./procesado
-
-for archivo in $entrada/*.txt #archivo es nuestra variable iteradora, recorremos todos los archivos de tipo txt
-	do
-		if [ -f "$archivo" ]; then  #si el archivo existe...
-			cat "$archivo">>"$salida" #lo leemos y copiamos en salida (recordando q es el FILENAME.txt)
-			mv "$archivo" "$procesado" #movemos el archivo a la carpeta procesado
-
-
-		fi
-	done
-  
+for archivo in $entrada/*.txt
+do
+if [ -f "$archivo" ];then
+cat "$archivo">>"$salida"
+mv "$archivo" "$procesado"
+fi
+done
--- carpetaMenu/menu.sh
+++ carpetaMenu/menu.sh
@@ -2,9 +2,12 @@
 
 archivo=$1
 
-export FILENAME=
+export FILENAME="alumnos"
 
 opcion=0
+
+
+
 
 until [ $opcion == "6" ]
 do
@@ -15,50 +18,79 @@ do
 	echo "4- Ver 10 Notas más Altas "
 	echo "5- Buscar Alumno por Padrón "
 	echo "6- SALIR "
+	echo "d- Borrar entorno y procesos"
 
 	echo  "Ingrese la opcion elegida: "
 	read opcion
 
-	case $opcion in
-		1)
-			mkdir ./Home
-			mkdir ./Home/EPNro1
-			mkdir ./Home/EPNro1/entrada
-			mkdir ./Home/EPNro1/salida
-			mkdir ./Home/EPNro1/procesado
-			echo "Entorno creado"
-			ls /*;;
+		case $opcion in
+			1)
+				mkdir ./Home
+				mkdir ./Home/EPNro1
+				mkdir ./Home/EPNro1/entrada
+				mkdir ./Home/EPNro1/salida
+				mkdir ./Home/EPNro1/procesado
+				
+				
+				touch ./Home/EPNro1/consolidar.sh				
+				echo '#!/bin/bash' > ./Home/EPNro1/consolidar.sh
+				echo '' >> ./Home/EPNro1/consolidar.sh
+				echo 'entrada=./entrada' >> ./Home/EPNro1/consolidar.sh
+				echo 'salida=./salida/FILENAME.txt' >> ./Home/EPNro1/consolidar.sh
+				echo 'procesado=./procesado' >> ./Home/EPNro1/consolidar.sh
+				echo 'for archivo in $entrada/*.txt' >> ./Home/EPNro1/consolidar.sh
+				echo 'do' >> ./Home/EPNro1/consolidar.sh
+				echo 'if [ -f "$archivo" ];then' >> ./Home/EPNro1/consolidar.sh
+				echo 'cat "$archivo">>"$salida"' >> ./Home/EPNro1/consolidar.sh
+				echo 'mv "$archivo" "$procesado"' >> ./Home/EPNro1/consolidar.sh
+				echo 'fi'>> ./Home/EPNro1/consolidar.sh
+				echo 'done' >> ./Home/EPNro1/consolidar.sh
 
-		2) 
-			
-			cd Home/EPNro1/
-			bash consolidar.sh &
-			echo "Archivos procesados"
-			;;
-		3)
-			cd Home/EPNro1/
-			if [ -f ./salida/FILENAME.txt ];then
-				sort -n ./salida/FILENAME.txt 
-			
-			fi;;
-		4) 	cd Home/EPNro1/
-                        if [ -f ./salida/FILENAME.txt ];then
-                                sort -n -r -k5 ./salida/FILENAME.txt | head -n 10 
+				chmod +x ./Home/EPNro1/consolidar.sh
 
-                        fi;;
+				echo "Entorno creado"
+				ls /*;;
 
-		5)
-			cd Home/EPNro1/
-			if [ -f ./salida/FILENAME.txt ]; then
-				echo "Ingrese N° de Padrón: "
-				read padron
-				grep $padron ./salida/FILENAME.txt
-			else
-				echo "El archivo no se encontró"
-			fi;; 
-		6)
-			echo "Adios";;
-		
-	esac
+			2) 
+				if [ -d ./Home ];then
+
+					cd Home/EPNro1/
+					bash consolidar.sh &
+					echo "Archivos procesados"
+
+				else
+					echo "Se necesita crear el Entorno"
+				fi;;
+			3)
+				cd Home/EPNro1/
+				if [ -f ./salida/FILENAME.txt ];then
+					sort -n ./salida/FILENAME.txt
+				else
+					echo "No se encontró el archivo" 
+				fi;;
+			4) 	cd ./Home/EPNro1/
+                	        if [ -f ./salida/FILENAME.txt ];then
+                        	        sort -n -r -k5 ./salida/FILENAME.txt | head -n 10
+				else
+                        	        echo "No se encontró el archivo" 
+
+                       		fi;;
+
+			5)
+				cd ./Home/EPNro1/
+				if [ -f ./salida/FILENAME.txt ]; then
+					echo "Ingrese N° de Padrón: "
+					read padron
+					grep $padron ./salida/FILENAME.txt
+				else
+					echo "El archivo no se encontró"
+				fi;; 
+			6)
+				echo "Adios";;
+			d)	
+				cd ./Home/
+        	        	rm -r ./EPNro1
+	        	        
+				;;
+		esac
 done
-
